f4e9af0dd761b9779d062a3e15e6915f9301da3f
Deolu_Josef <kiishijoseph@gmail.com>
2021-09-01 15:08:46 +0100

    worked with forms

--- index.js
+++ index.js
@@ -3,44 +3,36 @@ const path = require('path');
 const fs = require('fs');
 
 const { data } = require('./first');
+const { getReqData } = require('../events');
 
-const server = http.createServer((req, res) => {
-  switch (req.url) {
-    case '/':
-      let homeFile = path.join(__dirname, 'views/index.html');
-      let homePage = fs.readFileSync(homeFile, 'utf-8');
-      res.writeHead(200, { 'content-type': 'text/html' });
-      res.write(homePage);
-      res.end();
-      return;
-
-    case '/about':
-      let aboutFile = path.join(__dirname, 'views/about.html');
-      let aboutPage = fs.readFileSync(aboutFile, 'utf-8');
-      res.writeHead(200, { 'content-type': 'text/html' });
-      res.write(aboutPage);
-      res.end();
-      return;
-
-    case '/contact':
-      let contactFile = path.join(__dirname, 'views/contact.html');
-      let contactPage = fs.readFileSync(contactFile, 'utf-8');
-      res.writeHead(200, { 'content-type': 'text/html' });
-      res.write(contactPage);
-      res.end();
-      return;
-
-    case '/api/names':
-      res.writeHead(200, { 'content-type': 'application/json' });
-      res.write(JSON.stringify(data));
-      res.end();
-      return;
-
-    default:
-      res.writeHead(404, { 'content-type': 'text/html' });
-      res.write('<h1>Page not found </h1>');
-      res.end();
-      return;
+const server = http.createServer(async (req, res) => {
+  if (req.url === '/' && req.method === 'GET') {
+    console.log(req.method);
+    let homeFile = path.join(__dirname, 'views/index.html');
+    let homePage = fs.readFileSync(homeFile, 'utf-8');
+    res.writeHead(200, { 'content-type': 'text/html' });
+    res.write(homePage);
+    res.end();
+  } else if (req.url === '/about' && req.method === 'GET') {
+    let aboutFile = path.join(__dirname, 'views/about.html');
+    let aboutPage = fs.readFileSync(aboutFile, 'utf-8');
+    res.writeHead(200, { 'content-type': 'text/html' });
+    res.write(aboutPage);
+    res.end();
+  } else if (req.url === '/contact-us' && req.method === 'GET') {
+    let contactFile = path.join(__dirname, 'views/contact.html');
+    let contactPage = fs.readFileSync(contactFile, 'utf-8');
+    res.writeHead(200, { 'content-type': 'text/html' });
+    res.write(contactPage);
+    res.end();
+  } else if (req.url === '/contact-us' && req.method === 'POST') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    let body = await getReqData(req);
+    res.write(body)
+    res.end()
+  } else {
+    res.writeHead(404, { 'content-type': 'text/html' });
+    res.end('<h1>Page not found </h1>');
   }
 });
 
@@ -48,3 +40,9 @@ const port = 5000;
 server.listen(port, () => {
   console.log(`server is running on http://localhost:${port}`);
 });
+
+// case '/api/names':
+//   res.writeHead(200, { 'content-type': 'application/json' });
+//   res.write(JSON.stringify(data));
+//   res.end();
+//   return;
--- test.js
+++ test.js
@@ -1,37 +1,88 @@
+const fs = require('fs');
 const path = require('path');
 
-const fs = require('fs'); // file system
+let fileone = path.join(__dirname, '/files/my.txt');
+let fileTwo = path.join(__dirname, '/files/file2.txt');
 
-let fileOne = path.join(__dirname, 'files/one.txt');
-// console.log(fileOne);
+// synchronous
+// const dataOne = fs.readFileSync(fileone, 'utf-8');
+// const dataTwo = fs.writeFileSync(fileTwo, `my new data \n ${dataOne}`);
+// const dataThree = fs.readFileSync(fileTwo, 'utf-8');
+// console.log(dataThree);
 
-//blocking model/synchronous of reading files
-let dataOne = fs.readFileSync(fileOne, 'utf-8');
-let dataThree = fs.writeFileSync(
-  path.join(__dirname, 'files/my.txt'),
-  `my new data ${dataOne} 😊😊`
-);
-console.log(dataOne);
+// asynchronous
+// fs.readFile(fileone, 'utf-8', (err, dataOne) => {
+//   if (err) {
+//     throw err;
+//   }
 
-let fileTwo = path.join(__dirname, 'files/Two.txt');
-let dataTwo = fs.readFileSync(fileTwo, 'utf-8');
-console.log(dataTwo);
+//   fs.writeFile('./files/async.txt', dataOne, (err) => {
+//     if (err) {
+//       throw err;
+//     }
+//     // console.log(dataTwo);
 
-// const { data, getAge } = require('./first');
+//     fs.readFile('./files/async.txt', 'utf-8', (err, dataTwo) => {
+//       if (err) {
+//         throw err;
+//       }
+//       console.log(dataTwo);
 
-// let age = getAge(2000);
-// console.log(data);
-// console.log(age);
-
-// for (let index = 0; data < data.length; data++) {
-//   const element = data[index];
-//   console.log(element);
-// }
-
-// data.forEach((element, index) => {
-//   console.log(index + 1, element);
+//       fs.readdir('./files', (err, files) => {
+//         if (err) {
+//           throw err;
+//         }
+//         console.log(files);
+//       });
+//     });
+//   });
 // });
 
-// data.map((el, id) => {
-//   console.log({ ...el, id });
-// });
+const readFilePromise = (file) =>
+  new Promise((resolve, reject) => {
+    return fs.readFile(file, 'utf-8', (err, data) => {
+      if (err) reject(err);
+      resolve(data);
+    });
+  });
+
+const writeFilePromise = (pathtoFile, dataToWrite) =>
+  new Promise((resolve, reject) => {
+    return fs.writeFile(pathtoFile, dataToWrite, (err) => {
+      if (err) reject(err);
+      resolve('WRITE FILE SUCCESS');
+    });
+  });
+
+
+
+  //consume promise
+
+// readFilePromise(fileone)
+//   .then((result1) =>
+//     writeFilePromise(fileTwo, `New data from promise ${result1}`)
+//   )
+//   .then((result2) => readFilePromise(fileTwo))
+//   .then((result3) => console.log(result3))
+//   .catch((err) => console.log(err));
+
+// readFilePromise(fileTwo)
+//   .then((result2) => console.log(result2))
+//   .catch((err) => console.log(err));
+
+
+const fileProcess = async () => {
+  try {
+    let f1 = await readFilePromise(fileone)
+    let f2 = await writeFilePromise('./files/await.txt', f1)
+    let f3 = await readFilePromise('./files/await.txt')
+    console.log(f3)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+fileProcess()
+
+
+
