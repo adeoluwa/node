10d5365fb094c137baf74d3d3f682b4eabbb8e43
Deolu_Josef <kiishijoseph@gmail.com>
2021-09-08 10:07:10 +0100

    worked with morter, mongodb

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // import express
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 const homeRoutes = require('./routes/home.routes');
 const postsRoutes = require('./routes/posts.routes');
 
@@ -8,22 +9,19 @@ const postsRoutes = require('./routes/posts.routes');
 const app = express();
 
 // Post middleware
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // templating set up
 app.set('views', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-
 // static route middleware
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 // routes middleware
 app.use('/', homeRoutes);
-app.use('/post', postsRoutes)
+app.use('/post', postsRoutes);
 
 // app.get('/', (req, res, next) => {
 //   res.sendFile(path.join(__dirname, 'views/index.html'));
@@ -34,6 +32,22 @@ app.get('/api/names', (req, res, next) => {
     names: ['Ejike', 'Chinedu', 'Smart'],
   });
 });
+
+app.all('*', (req, res, next) => {
+  res.render('404.ejs', {
+    title: 'Error: 404',
+    data: 'Oops page not found',
+  });
+});
+
+mongoose
+  .connect('mongodb://localhost:27017/media', { useNewurlParser: true })
+  .then(() => {
+    console.log('database is connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -1,15 +1,54 @@
 const express = require('express');
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, 'uploads/pictures');
+  },
+  filename: (req, file, callback) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    const extname = path.extname(file.originalname);
+    callback(null, file.fieldname + '-' + uniqueSuffix + extname);
+  },
+});
+
+const upload = multer({
+  storage,
+  limits: { fileSize: 1000000 },
+  fileFilter: (req, file, callback) => {
+    console.log(file)
+    file.mimetype === 'image/jpeg' || file.mimetype === 'image/png';
+
+    if (file.mimetype === 'image/jpeg' || file.mimetype == 'image/png') {
+      callback(null, true);
+    } else {
+      callbacak(new Error('file type not supported upload jpeg or png'));
+    }
+  },
+});
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  console.log(req.query);
-  res.send('hey my post');
+  res.render('posts/list.ejs', {
+    title: 'posts',
+  });
 });
 
-router.get('/:name', (req, res, next) => {
-  console.log(req.params);
-  res.send('hey my post');
+router.get('/create', (req, res, next) => {
+  res.render('posts/create.ejs', {
+    title: 'create post',
+  });
 });
 
+router.post('/create', (req, res, next) => {
+  const image = upload.single('image');
+  image(req, res, (err) => {
+    if (err) {
+      return new Error(err);
+    }
+    console.log(req.file);
+  });
+});
 module.exports = router;
